939e5890ef80c9a9ac5395a506445b084d9ffb20
puckish-agent-6289 <manuelticusan@yahoo.com>
2019-02-04 21:52:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def badge_maker (name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  for i in 0..attendees.length-1
+   puts "Hello, my name is #{attendees[i]}."
+ end
+end

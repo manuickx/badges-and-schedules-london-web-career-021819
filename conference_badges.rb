6a16d0a08611e644dc7e6a1e30af1169ffffee16
puckish-agent-6289 <manuelticusan@yahoo.com>
2019-02-04 21:54:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,9 +2,6 @@ def badge_maker (name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
-attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  for i in 0..attendees.length-1
-   puts "Hello, my name is #{attendees[i]}."
- end
+def batch_badge_creator(attendees)
+ attendees.collect {|name| badge_maker(name)}
 end

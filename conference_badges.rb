568690eccb08c8160f2cbe41881f17d81791cc8c
puckish-agent-6289 <manuelticusan@yahoo.com>
2019-02-05 19:11:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,6 @@ def batch_badge_creator(attendees)
  attendees.collect {|name| badge_maker(name)}
 end
 
+def assign_rooms(attendees)
+ attendees.each_with_index.map {|name, index| "Hello, #{name}! You'll be assigned to room #{index+1}"}
+ end
